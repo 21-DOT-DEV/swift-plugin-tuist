5a507e42080a4883c09d82dfc7983a780c1ef43a
runner <runner@Mac-1754512586215.local>
2025-08-06 20:41:02 +0000

    Updating tuist to 4.58.1

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "tuist",
-            url: "https://github.com/21-DOT-DEV/swift-plugin-tuist/releases/download/4.58.0/tuist.artifactbundle.zip",
-            checksum: "02828c388db3f7433408d8b1dd8d4cafd7f59612afb5ffda640ec565a0e1fa87"
+            url: "https://github.com/21-DOT-DEV/swift-plugin-tuist/releases/download/4.58.1/tuist.artifactbundle.zip",
+            checksum: "4d228669d80f9a6b2051e6826d2f0766cf95742848bb6c93591e86999457c58e"
         ),
     ],
     swiftLanguageVersions: [.v5]
